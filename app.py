64b0b8bc210a4af6919b54a53604b12338f57025
Captain <papajiop11@gmail.com>
2026-03-11 12:59:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -610,7 +610,7 @@ Owner: {OWNER_USERNAME}
             if username:
                url = f"https://t.me/{username}"
             else:
-                url = f"https://t.me/{value}"
+               url = f"tg://user?id={value}"
 
             buttons.append(
                 [InlineKeyboardButton("👤 Open Telegram", url=url)]
